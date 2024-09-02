1e3e1ec3d408fb9a8593c383b0b88b2ebde5f974
RenoTxT <reno.mardiputra@gmail.com>
2024-09-02 16:53:08 +0700

    p0.1

--- src/components/maintenance.vue
+++ src/components/maintenance.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="container-fluid text-center">
     <h1>Website under maintenance</h1>
     <p>Copyright © 2024 Kana Group. All right reserved.</p>
   </div>
