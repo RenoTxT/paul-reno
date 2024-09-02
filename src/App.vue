1861d03eb2510b5333db6a473710b8b9b3ab6784
RenoTxT <reno.mardiputra@gmail.com>
2024-09-02 16:51:23 +0700

    p0.0

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import maintenance from './components/maintenance.vue'
+</script>
+
+<template>
+    <maintenance />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/maintenance.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Website under maintenance</h1>
+    <p>Copyright © 2024 Kana Group. All right reserved.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
